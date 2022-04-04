f349125328f80d192c8e8be3a15c4ed143d2ea06
Toni <eutrix@duck.com>
2022-04-04 14:07:06 +0200

    golubica

--- a.py
+++ a.py
@@ -1,6 +1,8 @@
 from PIL import Image
-file = Image.open('hlapic-big.png')
-scale = 8
+file = Image.open('golubica-big.png')
+
+scale = 16
+
 new = Image.new('RGBA', (file.width//scale+1, file.height//scale+1), (255,255,255,0))
 # go through each pixel of a file
 for x in range(6,file.width,scale):
@@ -8,5 +10,5 @@ for x in range(6,file.width,scale):
         #get pixel value
         pix = file.getpixel((x,y))
         new.putpixel(((x-6)//scale,(y-6)//scale), pix)
-new.save('hlapic-resized.png')
+new.save('golubica-resized.png')
 

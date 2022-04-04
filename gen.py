35003f136439ecbc8853bc420ced845b1f3e9c9a
Toni <eutrix@duck.com>
2022-04-04 11:19:14 +0200

    check for reddit colors

--- gen.py
+++ gen.py
@@ -1,5 +1,17 @@
 from PIL import Image
 
+reddit_colors = {"6d001a", "be0039", "ff4500", "ffa800", "ffd635", "fff8b8", "00a368", "00cc78", "7eed56", "00756f", "009eaa", "00ccc0", "2450a4", "3690ea", "51e9f4", "493ac1", "6a5cff", "94b3ff", "811e9f", "b44ac0", "e4abff", "de107f", "ff3881", "ff99aa", "6d482f", "9c6926", "ffb470", "000000", "515252", "898d90", "d4d7d9", "ffffff"}
+reddit_colors = set([tuple(int(h[i:i+2], 16) for i in (0, 2, 4)) for h in reddit_colors])
+
+def closest_color(rgb):
+    r, g, b = rgb
+    color_diffs = []
+    for color in reddit_colors:
+        cr, cg, cb = color
+        color_diff = (r - cr)**2 + (g - cg)**2 + (b - cb)**2
+        color_diffs.append((color_diff, color))
+    return min(color_diffs)[1]
+
 hr1 = ((73*3,717*3), Image.open('hr1.png'))
 hr2 = ((361*3, 890*3), Image.open('hr2.png'))
 zagi = ((1830*3, 952*3), Image.open('zagiflower1.png'))
@@ -19,6 +31,6 @@ for i in [hr1, hr2, zagi, kravata, modric]:
             #if pixel is not transparent
             if pix != (255,255,255,0):
                 #draw pixel on canvas
-                canvas.putpixel((x*3+x0-2,y*3+y0-2), pix)
+                canvas.putpixel((x*3+x0-2,y*3+y0-2), closest_color((pix[0], pix[1], pix[2])))
 
 canvas.save("canvas.png")

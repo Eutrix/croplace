3315adf52be2eec8ffa43039029291b4088a1a52
Toni <eutrix@duck.com>
2022-04-04 11:23:05 +0200

    fix transparency issues

--- gen.py
+++ gen.py
@@ -3,8 +3,7 @@ from PIL import Image
 reddit_colors = {"6d001a", "be0039", "ff4500", "ffa800", "ffd635", "fff8b8", "00a368", "00cc78", "7eed56", "00756f", "009eaa", "00ccc0", "2450a4", "3690ea", "51e9f4", "493ac1", "6a5cff", "94b3ff", "811e9f", "b44ac0", "e4abff", "de107f", "ff3881", "ff99aa", "6d482f", "9c6926", "ffb470", "000000", "515252", "898d90", "d4d7d9", "ffffff"}
 reddit_colors = set([tuple(int(h[i:i+2], 16) for i in (0, 2, 4)) for h in reddit_colors])
 
-def closest_color(rgb):
-    r, g, b = rgb
+def closest_color(r, g, b):
     color_diffs = []
     for color in reddit_colors:
         cr, cg, cb = color
@@ -31,6 +30,6 @@ for i in [hr1, hr2, zagi, kravata, modric]:
             #if pixel is not transparent
             if pix != (255,255,255,0):
                 #draw pixel on canvas
-                canvas.putpixel((x*3+x0-2,y*3+y0-2), closest_color((pix[0], pix[1], pix[2])))
+                canvas.putpixel((x*3+x0-2,y*3+y0-2), closest_color(pix[0], pix[1], pix[2]) + (pix[3],))
 
 canvas.save("canvas.png")

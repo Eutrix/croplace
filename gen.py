bc6c4100ccf132bb1ddedeea300759e667d2218c
Toni <eutrix@duck.com>
2022-04-04 12:31:57 +0200

    zagi moved a bit

--- gen.py
+++ gen.py
@@ -13,7 +13,7 @@ def closest_color(r, g, b):
 
 hr1 = ((73*3,717*3), Image.open('hr1.png'))
 hr2 = ((361*3, 890*3), Image.open('hr2.png'))
-zagi = ((352*3, 1070*3), Image.open('zagiflower.png'))
+zagi = ((352*3, 1072*3), Image.open('zagiflower.png'))
 kravata = ((434*3, 912*3), Image.open('kravata2.png'))
 modric = ((330*3, 1088*3), Image.open('modric-resized-edited.png'))
 
